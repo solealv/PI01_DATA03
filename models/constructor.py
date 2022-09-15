3d88087b25fc10ffebe4d2caf792c80d6071d498
solealv <ms.alvarez.iq@gmail.com>
2022-09-15 19:29:21 +0200

    correccion columna url

--- models/constructor.py
+++ models/constructor.py
@@ -5,7 +5,8 @@ from config.db import meta, engine
 constructors = Table('constructor', meta, Column('constructorId', Integer, primary_key=True), 
                                 Column('constructorRef', String(100)),
                                 Column('name', String(100)),
-                                Column('nationality', Text))
+                                Column('nationality', String(100)),
+                                Column('url', Text))
 
 
 meta.create_all(engine)
